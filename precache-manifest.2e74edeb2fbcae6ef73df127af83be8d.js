b1ac976a7ba1c9958ea217ac6b3769c6ccc2e70e
Divyansh Gandhi <divyansh50g@gmail.com>
2019-12-09 15:40:49 +0530

    Updates

---
+++ precache-manifest.2e74edeb2fbcae6ef73df127af83be8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e616fb210542f11db29c6c7db01f1587",
+    "url": "/test-reactt/index.html"
+  },
+  {
+    "revision": "f10760156663d1aab450",
+    "url": "/test-reactt/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f802fb0c2f6316e56432",
+    "url": "/test-reactt/static/js/2.32a64079.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-reactt/static/js/2.32a64079.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f10760156663d1aab450",
+    "url": "/test-reactt/static/js/main.36acfb7a.chunk.js"
+  },
+  {
+    "revision": "71f5f9463a6cc52e5cf9",
+    "url": "/test-reactt/static/js/runtime-main.f3b09e2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-reactt/static/media/logo.5d5d9eef.svg"
+  }
+]);
